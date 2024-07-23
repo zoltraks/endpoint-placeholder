17f467984d4bc48a9d6dae106fa1681bac6922dc
Zoltraks <f.golewski@gmail.com>
2024-07-23 19:29:32 +0200

    body response in failure

--- app/failure.php
+++ app/failure.php
@@ -9,8 +9,20 @@ if (isset($_GET['code'])) {
     }
 }
 
+header('X-Request-Method: ' . $_SERVER['REQUEST_METHOD']);
+
 http_response_code($code);
 
 if (isset($_GET['with'])) {
     echo $_GET['with'];
+} else {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
+        $payload = $_POST;
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES) && !empty($_FILES)) {
+        $payload = $_FILES;
+    } else {
+        $payload = file_get_contents('php://input');
+    }
+    echo $payload;
 }
+
